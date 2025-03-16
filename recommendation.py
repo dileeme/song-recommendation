a72ad114ceded19edf6d1925cc254693d5bb070b
dilen shankar <elchurrocuandotebese@gmail.com>
2025-03-16 21:35:28 +0530

    Update recommendation.py

--- recommendation.py
+++ recommendation.py
@@ -1,3 +1,14 @@
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+
+def load_dataset(csv_path):
+  df = pd.read_csv(csv_path)
+  return df
+
+def train_model(df):
+  vectorizer = TfidfVectorizer(stop_words='english')
+  song_vectors = vectorizer.fit_transform(df['title'])
+  return vectorizer, song_vectors
+
+
